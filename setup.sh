78be5e95c26b379e3252570f4f9824018386b207
susuky <hpisj322@gamil.com>
2024-01-29 17:48:01 +0800

    add build wheel option

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
+
+# Args:
+    # BUILD_WHEEL: 1 or 0
+    # CYTHONIZE: 1 or 0
+
+# For example:
+    # $ CYTHONIZE=1 BUILD_WHEEL=1 ./setup.sh
+    # $ pip install dist/trtify-*.whl
+
 rm dist -rf
 
-# python setup.py bdist_wheel
-# pip install dist/trtify-*.whl
 
-if [ -n "$CYTHONIZE" ]; then
-    CYTHONIZE=$CYTHONIZE pip install .
+if [ "$BUILD_WHEEL" = '1' ]; then
+    python setup.py bdist_wheel 
+    # pip install dist/trtify-*.whl
 else
-    pip install .
+    pip install . 
 fi
+
+
