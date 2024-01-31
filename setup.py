e3f0f4e581082d99f157527ff81d0a67afcb9ff8
susuky <hpisj322@gamil.com>
2024-02-01 01:29:20 +0800

    move source code to src

--- setup.py
+++ setup.py
@@ -1,43 +1,51 @@
 
 import os
-import shutil
 
-from Cython.Build import cythonize
 from setuptools import setup, Extension, find_packages  
 
 VERSION = '0.0.1'
 CYTHONIZE = os.getenv('CYTHONIZE', '0')
 
 if CYTHONIZE.lower() in ('1', 'true'):
-    modules = [
-        'builder',
-        'cuda_backend',
-        'encryption',
-        'network_definition',
-        'runtime',
-        'utils',
-        '__init__'
+    import atexit
+    import shutil
+    from Cython.Build import cythonize
+    from pathlib import Path
+
+
+    def clean(root='src', suffixes=('.pyx', '.c')):
+        for suffix in suffixes:
+            for path in Path(root).rglob(f'*{suffix}'):
+                path.unlink()
+    atexit.register(clean)
+
+
+    module_paths = [
+        *Path('src').rglob('*.py'),
     ]
 
-    for module in modules:
-        shutil.copy(f'trtify/{module}.py', f'trtify/{module}.pyx')
+    extensions = []
+    for path in module_paths:
+        path_pyx = path.with_suffix('.pyx')
+        shutil.copy(path, path_pyx)
 
-    extensions = [
-        Extension(f'trtify.{module}', [f'trtify/{module}.pyx'])
-        for module in modules
-    ]
+        module = '.'.join((*path.parts[1:-1], path.stem))
+        extensions.append(Extension(module, [path_pyx.as_posix()]))
 
     setup(
         name='trtify', 
         python_requires='>=3.8',
         ext_modules=cythonize(extensions), 
-        version=VERSION,
+        version=f'{VERSION}+cythonized',
     )
+
 else:
     setup(
         name='trtify', 
         python_requires='>=3.8',
         version=VERSION,
-        packages=find_packages(),
+        package_dir={'': 'src'},
+        packages=find_packages(where='src', include=['trtify', 'trtify.*']),
     )    
+
 
